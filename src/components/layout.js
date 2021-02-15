90934df16971e4613aff9be32abd3e97689207b4
mostlyclicks <carlos@mostlyclicks.com>
2021-02-14 17:27:37 -0700

    fix layout - take away HTML fragment tags

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,33 +7,19 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
 import Footer from "./footer"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+
 
   return (
-    <>
-      {/* <Header siteTitle={data.site.siteMetadata?.title || `Title`} /> */}
-      <div
-
-      >
-        <main>{children}</main>
-        <Footer />
-      </div>
-    </>
+     
+    <div>
+      <main>{children}</main>
+      <Footer />
+    </div>
+    
   )
 }
 
