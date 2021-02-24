0f15ec642a7fc4f3262e4190a44adf85ca95734b
Carlos Interone <carlos@mostlyclicks.com>
2021-02-24 07:24:57 -0700

    add google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Hulse Dental of Onalaska WI`,
+    description: `Learn more about Hulse Dental, our services and our office located in Onalaska WI`,
+    author: `mostlyClicks`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,11 +24,22 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-postcss`,
     `gatsby-plugin-scroll-reveal`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Noto Sans JP\:100,300,400,700`
+
+
+        ],
+        display: 'swap'
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/components/content-fs-text-block.js
+++ src/components/content-fs-text-block.js
@@ -5,7 +5,7 @@ const FSTextBlock = ({children, bgColor, textColor}) => {
     return (
         <div className={'flex ' + bgColor}>
             <div className={
-                'w-full mx-auto my-2 sm:w-8/12 md:my-12 md:w-6/12 lg:w-5/12 xl:w-4/12 '
+                'w-full mx-auto my-2 sm:w-10/12 md:my-12 md:w-8/12 lg:w-8/12 xl:w-6/12 '
                 + textColor
             }>
                 {children}
