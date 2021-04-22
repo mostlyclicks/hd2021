cb312b5e1385ba183db98576677d67d64133830d
mostlyclicks <carlos@mostlyclicks.com>
2021-04-21 19:39:59 -0600

    add staff

--- src/components/staff.js
+++ src/components/staff.js
@@ -6,6 +6,9 @@ import StaffJeanne from "../images/Staff-Jeanne-ClinicManager.jpg"
 import StaffAlisha from "../images/Staff-Alisha-Dental-Assistant.jpg"
 import StaffAmy from "../images/Staff-Amy-Hygienist.jpg"
 import StaffSandra from "../images/Staff-Sandra-Dental-Hygienist.jpg"
+import StaffSarah from "../images/Staff-SarahPic2.jpg"
+import StaffTara from "../images/Staff-TaraPic2b.jpg"
+import StaffKate from "../images/KateFPO.jpg"
 
 const Staff = () => {
 
@@ -13,12 +16,14 @@ const Staff = () => {
     <div className=" staff my-18">
         <h3 className="text-center">Our Staff</h3>
         <div className="flex flex-wrap lg:container">
-        <figure><img src={StaffKurt} alt="Dr. Kurt Hulse"/><figcaption>Dr. Kurt Hulse</figcaption></figure>
         <figure><img src={StaffJane} alt="Jane - Office Manager"/><figcaption>Jane - Office Manager</figcaption></figure>
         <figure><img src={StaffJeanne} alt="Jeanne - Clinic Manager"/><figcaption>Jeanne - Clinic Manager</figcaption></figure>
         <figure><img src={StaffAlisha} alt="Alisha - Dental Assistant"/><figcaption>Alisha - Dental Assistant</figcaption></figure>
         <figure><img src={StaffAmy} alt="Amy - Dental Hygienist"/><figcaption>Amy - Dental Hygienist</figcaption></figure>
         <figure><img src={StaffSandra} alt="Sandra - Dental Hygienist"/><figcaption>Sandra - Dental Hygienist</figcaption></figure>
+        <figure><img src={StaffSarah} alt="Sarah - Dental Hygienist"/><figcaption>Sarah - Dental Hygienist</figcaption></figure>
+        <figure><img src={StaffTara} alt="Tara - Dental Hygienist"/><figcaption>Tara - Dental Hygienist</figcaption></figure>
+        <figure><img src={StaffKate} alt="Kate - Assistant"/><figcaption>Kate - Assistant</figcaption></figure>
         </div>
     </div>
 
--- src/pages/our-team.js
+++ src/pages/our-team.js
@@ -5,7 +5,7 @@ import Navbar from '../components/navbar'
 import HeroLevel2 from '../components/hero-level-2'
 import HeroImage from '../images/DSC-0260.jpg'
 import ContentTextPict from '../components/content-text-pict'
-import KurtHulse from '../images/kurt_home.jpg'
+import KurtHulse from '../images/Staff-Kurt-Hulse.jpg'
 import Payment from '../components/payment'
 import FSTextBlock from "../components/content-fs-text-block"
 import Staff from "../components/staff"
@@ -22,16 +22,16 @@ const DrKurtHulse = () => {
             <Navbar transparent />
             <HeroLevel2 title="Our Team" image={HeroImage} minHeight="100vh" className="bm-10"/>
             <a id="section-2"></a>
-            <FSTextBlock lgContainer customClasses=" md:mt-16">
-                <p>Dr. Hulse is an Onalaska local. He went on to study at Luther College in Decorah, IA and attended the University of Minnesota Dental School. Upon graduation, Dr. Hulse was admitted to the General Practice Residency program at Balboa Naval Hospital. His love of the Coulee Region brought him back to the area where he established Hulse Dental as a center for family dental care, restorative dentistry (fillings and crowns), and cosmetic dentistry, including teeth whitening, endodontic (root canal) &amp; orthodontic (braces/Invisalign) services. Dr Hulse is an active member of the Academy of Cosmetic Dentistry, a past professor at the University of Minnesota Dental School, former President of the La Crosse District Dental Society and the recent Director of the La Crosse area Give Kids a Smile Program for 15 years. Dr. Hulse and his wife Jeanne live in Onalaska and have three sons, Kyle, Garrett and Calvin. He has a love of learning, and in his free-time enjoys spending time with his family, creating art, and enjoys a passion for the outdoors, sports and travel.</p>
+            <FSTextBlock lgContainer customClasses="md:mt-16">
+                <p className="">Dr. Hulse is an Onalaska local. He went on to study at Luther College in Decorah, IA and attended the University of Minnesota Dental School. Upon graduation, Dr. Hulse was admitted to the General Practice Residency program at Balboa Naval Hospital. His love of the Coulee Region brought him back to the area where he established Hulse Dental as a center for family dental care, restorative dentistry (fillings and crowns), and cosmetic dentistry, including teeth whitening, endodontic (root canal) &amp; orthodontic (braces/Invisalign) services. Dr Hulse is an active member of the Academy of Cosmetic Dentistry, a past professor at the University of Minnesota Dental School, former President of the La Crosse District Dental Society and the recent Director of the La Crosse area Give Kids a Smile Program for 15 years. Dr. Hulse and his wife Jeanne live in Onalaska and have three sons, Kyle, Garrett and Calvin. He has a love of learning, and in his free-time enjoys spending time with his family, creating art, and enjoys a passion for the outdoors, sports and travel.</p>
             </FSTextBlock>
 
-            <section className="md:flex md:container mx-auto p-dark border-t">
-                <div className=" justify-center self-center px-6">
+            <section className="md:flex md:container md:flex-col lg:flex-row mx-auto p-dark border-t">
+                <div className=" justify-center mt-7 px-6 md:w-full lg:w-1/3">
                     <img src={KurtHulse} alt="Kurt Hulse"  /> 
                 </div>
-                <div className="pb-6 md:p-6  text-base">
-                <h3>Education</h3>
+                <div className="pb-6 p-6  text-base lg:w-1/3">
+                <h3 className="text-lg">Education</h3>
                 <ul className="list-outside list-disc ml-4">
                     <li>Onalaska High School - Onalaska WI</li>
                     <li>Luther College, Decorah IA 1986 - B.A. in Biology, Minor in Chemistry</li>
@@ -40,8 +40,8 @@ const DrKurtHulse = () => {
                     <li>University of Minnesota Dental School 1993-2000 - Clinical Instructor</li>
                 </ul>
                 </div>
-                <div className="md:p-6  text-base">
-                    <h3>Professional Associations</h3>
+                <div className="p-6  text-base lg:w-1/3">
+                    <h3 className="text-lg">Professional Associations</h3>
                     <ul className="list-outside list-disc ml-4">
                     <li>President, La Crosse District Dental Society</li>
                     <li>Wisconsin Dental Association</li>
