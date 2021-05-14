9a3210a726077bcec93a87b970ce3475a132e549
mostlyclicks <carlos@mostlyclicks.com>
2021-05-14 08:43:30 -0600

    staff text size

--- src/components/staff-component.js
+++ src/components/staff-component.js
@@ -6,7 +6,7 @@ const StaffComponent = ({name, image, description, bgColor}) => {
   return (
     <StyledFigure>
       <img src={image} alt={name} /><figcaption>{name}</figcaption>
-      <div className="absolute top-0 left-0 p-5 bg-opacity-90 h-full text-white" style={{backgroundColor:bgColor}}>{description}</div>
+      <div className="absolute top-0 left-0 p-5 bg-opacity-90 h-full text-sm sm:text-base text-white" style={{backgroundColor:bgColor}}>{description}</div>
     </StyledFigure>
   )
 
