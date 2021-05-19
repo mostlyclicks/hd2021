461b685750bf50d05c4be5a00be48708ab2bc0e6
mostlyclicks <carlos@mostlyclicks.com>
2021-05-19 05:42:39 -0600

    add map linkStyles

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
     {
       resolve:`gatsby-plugin-react-leaflet`,
       // options: {
-      //   linkStyles: true // (default: true) Enable/disable loading stylesheets via CDN
+        linkStyles: true // (default: true) Enable/disable loading stylesheets via CDN
       // }
     },
     
