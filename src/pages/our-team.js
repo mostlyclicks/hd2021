edfc8e016ee1091ab77da8478aef1adb7891ac40
mostlyclicks <carlos@mostlyclicks.com>
2021-06-06 19:44:38 -0600

    start our-team

--- src/pages/our-team.js
+++ src/pages/our-team.js
@@ -2,28 +2,37 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Navbar from '../components/navbar'
+import { RichText } from "prismic-reactjs"
 import HeroLevel2 from '../components/hero-level-2'
 import HeroImage from '../images/DSC-0260.jpg'
-import ContentTextPict from '../components/content-text-pict'
 import KurtHulse from '../images/Staff-Kurt-Hulse.jpg'
 import Payment from '../components/payment'
 import FSTextBlock from "../components/content-fs-text-block"
 import Staff from "../components/staff"
 
 
-const DrKurtHulse = () => {
+const DrKurtHulse = ({data}) => {
+
+    const document = data.allPrismicPage.nodes[0].data
+
+    const seoTitle = document.seo_page_title
+    const seoDescription = document.seo_meta_description
+
+    const mainText = document.body[0].primary.text
+    console.log(mainText)
+
 
     return (
         <Layout>
             <SEO 
-                title="Our Team | Hulse Dental, Onalaska WI"
-                description="Learn more about Dr. Kurt Hulse and the team at Hulse Dental in Onalaska Wisconsin."
+                title={seoTitle}
+                description={seoDescription}
             />
             <Navbar transparent />
             <HeroLevel2 title="Our Team" image={HeroImage} minHeight="100vh" className="bm-10"/>
             <a id="section-2"></a>
             <FSTextBlock lgContainer customClasses="md:mt-16">
-                <p className="">Dr. Hulse is an Onalaska local. He went on to study at Luther College in Decorah, IA and attended the University of Minnesota Dental School. Upon graduation, Dr. Hulse was admitted to the General Practice Residency program at Balboa Naval Hospital. His love of the Coulee Region brought him back to the area where he established Hulse Dental as a center for family dental care, restorative dentistry (fillings and crowns), and cosmetic dentistry, including teeth whitening, endodontic (root canal) &amp; orthodontic (braces/Invisalign) services. Dr Hulse is an active member of the Academy of Cosmetic Dentistry, a past professor at the University of Minnesota Dental School, former President of the La Crosse District Dental Society and the recent Director of the La Crosse area Give Kids a Smile Program for 15 years. Dr. Hulse and his wife Jeanne live in Onalaska and have three sons, Kyle, Garrett and Calvin. He has a love of learning, and in his free-time enjoys spending time with his family, creating art, and enjoys a passion for the outdoors, sports and travel.</p>
+                <RichText render={mainText.raw} />
             </FSTextBlock>
 
             <section className="md:flex md:container md:flex-col lg:flex-row mx-auto p-dark border-t">
@@ -67,3 +76,44 @@ const DrKurtHulse = () => {
 }
 
 export default DrKurtHulse
+
+export const query = graphql`
+query {
+  allPrismicPage(filter: {uid: {eq: "our-team"}}) {
+    nodes {
+      data {
+        seo_meta_description
+        seo_page_title
+        body {
+          ... on PrismicPageBodyTextImageSection {
+            id
+            primary {
+              image {
+                alt
+                fluid {
+                  src
+                }
+              }
+              section_text {
+                html
+              }
+              section_title {
+                text
+              }
+            }
+          }
+          ... on PrismicPageBodyText {
+            id
+            primary {
+              text {
+                raw
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+`
