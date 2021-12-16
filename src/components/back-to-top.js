b0c79284ebb4bbd280a8c888ed08d7c10d2619a5
mostlyclicks <carlos@mostlyclicks.com>
2021-12-16 12:31:34 -0700

    button styling

--- src/components/back-to-top.js
+++ src/components/back-to-top.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const backToTop = () => {
   return (
-    <div>
-      <a href="#top">Back to Top</a>
+    <div className="flex justify-center bg-gray-100 p-8">
+      <a href="#top" className="bg-purple-200 block py-3 px-24  hover:bg-purple-600 hover:text-gray-50">Back to Top</a>
     </div>
   )
 }
