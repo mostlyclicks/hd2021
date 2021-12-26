b2617fd4407fd794cffcc071bd7f3c9b3a0f148c
mostlyclicks <carlos@mostlyclicks.com>
2021-12-26 16:03:53 -0700

    correct form link to static

--- src/components/dental-forms.js
+++ src/components/dental-forms.js
@@ -5,10 +5,10 @@ const DentalForms = () => {
   return (
       
     <ul>
-      <li><a href="New-Patient-Registration-full.pdf">New Patient Form</a></li>
-      <li><a href="https://hd10.s3.amazonaws.com/Forms/Medical-History-update.pdf">Medical History</a></li>
+      <li><a href={'/New-Patient-Registration-full.pdf'}>New Patient Form</a></li>
+      <li><a href="https://hd10.s3.amazonaws.com/Forms/Medical-History-update.pdf">Medical History Update</a></li>
       <li><a href="HIPAA.pdf">Health Information Consent and Use Form</a></li>
-      <li><a href="HD-Financial-Policy-full.pdf">Financial Policy</a></li>
+      <li><a href={'/HD-Financial-Policy-full.pdf'}>Financial Policy</a></li>
     </ul>
   )
 }
