192e6d17b719df506eca6d6e7eead6e9e127c450
mostlyclicks <carlos@mostlyclicks.com>
2021-12-26 16:09:11 -0700

    fix hipaa form link

--- src/components/dental-forms.js
+++ src/components/dental-forms.js
@@ -7,7 +7,7 @@ const DentalForms = () => {
     <ul>
       <li><a href={'/New-Patient-Registration-full.pdf'}>New Patient Form</a></li>
       <li><a href="https://hd10.s3.amazonaws.com/Forms/Medical-History-update.pdf">Medical History Update</a></li>
-      <li><a href="HIPAA.pdf">Health Information Consent and Use Form</a></li>
+      <li><a href={'/HIPAA.pdf'}>Health Information Consent and Use Form</a></li>
       <li><a href={'/HD-Financial-Policy-full.pdf'}>Financial Policy</a></li>
     </ul>
   )
