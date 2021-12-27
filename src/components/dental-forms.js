1306282f5b658b928a828b12227af97082448842
mostlyclicks <carlos@mostlyclicks.com>
2021-12-27 09:40:41 -0700

    add form icons to dental forms

--- src/components/dental-forms.js
+++ src/components/dental-forms.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import { FaFilePdf } from 'react-icons/fa'
 
 const DentalForms = () => {
 
   return (
       
-    <ul>
-      <li><a href={'/New-Patient-Registration-full.pdf'}>New Patient Form</a></li>
-      <li><a href="https://hd10.s3.amazonaws.com/Forms/Medical-History-update.pdf">Medical History Update</a></li>
-      <li><a href={'/HIPAA.pdf'}>Health Information Consent and Use Form</a></li>
-      <li><a href={'/HD-Financial-Policy-full.pdf'}>Financial Policy</a></li>
+    <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+      <li className="text-base rounded-lg block border-2 p-4 flex flex-1 sm:flex-col items-center">
+        <FaFilePdf className="text-7xl mb-4"/>
+      <a href={'/New-Patient-Registration-full.pdf'}>New Patient Form</a></li>
+
+      <li className="text-base rounded-lg block border-2 p-4 flex flex-1 sm:flex-col items-center">
+      <FaFilePdf className="text-7xl mb-4"/><a href="https://hd10.s3.amazonaws.com/Forms/Medical-History-update.pdf" className="text-center">Medical History Update</a></li>
+
+      <li className="text-base rounded-lg block border-2 p-4 flex flex-1 sm:flex-col items-center">
+      <FaFilePdf className="text-7xl mb-4"/><a href={'/HIPAA.pdf'} className="text-center">Health Information Consent and Use Form</a></li>
+
+      <li className="text-base rounded-lg block border-2 p-4 flex flex-1 sm:flex-col items-center">
+      <FaFilePdf className="text-7xl mb-4"/><a href={'/HD-Financial-Policy-full.pdf'}>Financial Policy</a></li>
     </ul>
   )
 }
